6737315058fe1ec4988d685cdb4c6fcb044e9475
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-08-08 12:05:38 +0530

    kick off 0.4.1 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bract/bract.ring "0.4.0"
+(defproject bract/bract.ring "0.4.1-SNAPSHOT"
   :description "Bract module for Ring support"
   :url "https://github.com/bract/bract.ring"
   :license {:name "Eclipse Public License"
