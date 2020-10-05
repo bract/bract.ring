1ddb3a0b3d490b85edb45928087975fc494e7e19
Shantanu Kumar <kumar.shantanu@gmail.com>
2020-10-05 08:27:02 +0530

    remove unusable arity

--- src/bract/ring/middleware.clj
+++ src/bract/ring/middleware.clj
@@ -21,41 +21,39 @@
   |`:request-logger`  |`(fn [request])`                                  | Request logger function   | No-op   |
   |`:response-logger` |`(fn [request response ^double duration-millis])` | Response logger function  | No-op   |
   |`:exception-logger`|`(fn [request exception ^double duration-millis])`| Exception logger function | No-op   |"
-  ([handler]
-    (traffic-log-middleware handler {}))
-  ([handler {:keys [request-logger
-                    response-logger
-                    exception-logger]
-             :or {request-logger (fn [request])
-                  response-logger (fn [request response ^double duration-millis])
-                  exception-logger (fn [request exception ^double duration-millis])}
-             :as options}]
-    (fn logging-handler
-      ([request]
-        (request-logger request)
-        (let [start-nanos (System/nanoTime)]
-          (try
-            (let [response (handler request)]
-              (response-logger request response (util/elapsed-millis start-nanos))
-              response)
-            (catch Throwable e
-              (exception-logger request e (util/elapsed-millis start-nanos))
-              (throw e)))))
-      ([request respond raise]
-        (request-logger request)
-        (let [start-nanos (System/nanoTime)
-              vresponse (volatile! nil)
-              vcaught   (volatile! nil)]
-          (try
-            (handler request #(vreset! vresponse %) #(vreset! vcaught %))
-            (catch Throwable e
-              (exception-logger request e (util/elapsed-millis start-nanos))
-              (throw e)))
-          (let [duration (util/elapsed-millis start-nanos)]
-            (if-some [caught @vcaught]
-              (do
-                (exception-logger request caught duration)
-                (raise caught))
-              (let [response @vresponse]
-                (response-logger request response duration)
-                (respond response)))))))))
+  [handler {:keys [request-logger
+                   response-logger
+                   exception-logger]
+            :or {request-logger (fn [request])
+                 response-logger (fn [request response ^double duration-millis])
+                 exception-logger (fn [request exception ^double duration-millis])}
+            :as options}]
+  (fn logging-handler
+    ([request]
+      (request-logger request)
+      (let [start-nanos (System/nanoTime)]
+        (try
+          (let [response (handler request)]
+            (response-logger request response (util/elapsed-millis start-nanos))
+            response)
+          (catch Throwable e
+            (exception-logger request e (util/elapsed-millis start-nanos))
+            (throw e)))))
+    ([request respond raise]
+      (request-logger request)
+      (let [start-nanos (System/nanoTime)
+            vresponse (volatile! nil)
+            vcaught   (volatile! nil)]
+        (try
+          (handler request #(vreset! vresponse %) #(vreset! vcaught %))
+          (catch Throwable e
+            (exception-logger request e (util/elapsed-millis start-nanos))
+            (throw e)))
+        (let [duration (util/elapsed-millis start-nanos)]
+          (if-some [caught @vcaught]
+            (do
+              (exception-logger request caught duration)
+              (raise caught))
+            (let [response @vresponse]
+              (response-logger request response duration)
+              (respond response))))))))
